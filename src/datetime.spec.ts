3b46eddb55f6d2c5c4d99383cba98f5c8c87b5c8
Russ Biggs <russbiggs@gmail.com>
2025-05-22 17:53:35 -0600

    refactor datetime with luxon (#9)
    * refactor datetime with luxon
    
    * datetime updates and tests
    
    * additional tests
    
    * add support for Datetime type as input of Datetime
    
    * Added a few methods and tests
    
    * Some variable names changed
    
    ---------
    
    Co-authored-by: Christian Parker <c.andrew.parker@gmail.com>

---
+++ src/datetime.spec.ts
@@ -0,0 +1,88 @@
+import { expect, test } from 'vitest';
+import { Datetime } from './datetime';
+
+test('ISO format parses correctly by default', () => {
+    const datetime = new Datetime('2025-01-01T00:00:00-07:00')
+    expect(datetime.toUTC()).toBe('2025-01-01T07:00:00.000Z');
+});
+
+test('datetime string, with no timezone parse correctly from UTC to UTC equivalent tiemzone', () => {
+    const datetime = new Datetime('2025-01-01 00:00', { format: 'yyyy-MM-dd HH:mm', timezone: 'UTC', locationTimezone: 'Africa/Banjul' })
+    expect(datetime.toString()).toBe('2025-01-01T00:00:00.000+00:00');
+});
+
+test('UTC datetime correctly outputs to different timezone', () => {
+    const datetime = new Datetime('2025-01-01 00:00', { format: 'yyyy-MM-dd HH:mm', timezone: 'UTC', locationTimezone: 'America/Denver' })
+    expect(datetime.toString()).toBe('2024-12-31T17:00:00.000-07:00');
+});
+
+test('Datetime correctly outputs to location timezone', () => {
+    const datetime = new Datetime('2025-01-01 00:00', { format: 'yyyy-MM-dd HH:mm', timezone: 'America/Denver', locationTimezone: 'America/Denver' })
+    expect(datetime.toString()).toBe('2025-01-01T00:00:00.000-07:00');
+});
+
+test('Datetime correctly outputs to UTC timezone', () => {
+    const datetime = new Datetime('2025-01-01 00:00', { format: 'yyyy-MM-dd HH:mm', timezone: 'America/Denver', locationTimezone: 'America/Denver' })
+    expect(datetime.toUTC()).toBe('2025-01-01T07:00:00.000Z');
+});
+
+test('Local datetime correctly outputs to local timezone when locationTimezone not included', () => {
+    const datetime = new Datetime('2025-01-01 00:00', { format: 'yyyy-MM-dd HH:mm', timezone: 'America/Denver' })
+    expect(datetime.toString()).toBe('2025-01-01T00:00:00.000-07:00');
+});
+
+test('datetime string with Z correctly throws when timezone is also included', () => {
+    expect(() => new Datetime('2025-01-01 00:00Z', { format: 'yyyy-MM-dd HH:mmZ', timezone: 'America/Denver', locationTimezone: 'America/Denver' })).toThrow(TypeError)
+});
+
+test('unix timestamp number (seconds) parses correctly', () => {
+    const datetime = new Datetime(1746736701)
+    expect(datetime.toString()).toBe('2025-05-08T20:38:21.000Z');
+});
+
+test('unix timestamp number (second) parses and is transformed to custom timezone', () => {
+    const datetime = new Datetime(1746736701, { locationTimezone: 'America/Denver' })
+    expect(datetime.toString()).toBe('2025-05-08T14:38:21.000-06:00');
+});
+
+test('isGreaterThan works', () => {
+    const datetime1 = new Datetime(1746736701)
+    const datetime2 = new Datetime(1746736700)
+
+    expect(datetime1.isGreaterThan(datetime2)).toBeTruthy();
+});
+
+test('isLessThan works', () => {
+    const datetime1 = new Datetime(1746736701)
+    const datetime2 = new Datetime(1746736700)
+
+    expect(datetime2.isLessThan(datetime1)).toBeTruthy();
+});
+
+test('lesserOf works', () => {
+    const datetime1 = new Datetime(1746736701);
+    const datetime2 = new Datetime(1746736700);
+    expect(datetime2.lesserOf(datetime1)).toEqual(datetime2);
+});
+
+test('lesserOf works when values equal', () => {
+    const datetime1 = new Datetime(1746736700);
+    const datetime2 = new Datetime(1746736700);
+    expect(datetime2.lesserOf(datetime1)).toEqual(datetime2);
+});
+
+test('lesserOf works when value is null', () => {
+    const datetime2 = new Datetime(1746736700);
+    expect(datetime2.lesserOf(null)).toEqual(datetime2);
+});
+
+test('greaterOf works when values equal', () => {
+    const datetime1 = new Datetime(1746736700);
+    const datetime2 = new Datetime(1746736700);
+    expect(datetime1.greaterOf(datetime2)).toEqual(datetime1);
+})
+
+test('greaterOf works when value is null', () => {
+    const datetime2 = new Datetime(1746736700);
+    expect(datetime2.greaterOf(null)).toEqual(datetime2);
+})
--- src/datetime.ts
+++ src/datetime.ts
@@ -1,44 +1,100 @@
-import { tz } from "@date-fns/tz";
-import { fromUnixTime, parse, parseISO, format } from "date-fns";
+import { DateTime } from 'luxon';
 
+
+
+interface DatetimeOptionsDefinition {
+  format?: string,
+  timezone?: string,
+  locationTimezone?: string
+}
+
+// settled on `timezone` as the variable name of the formated string to match the variable `format`
+// both referring to the string as written in the data
 export class Datetime {
-
-    #input: string | number | Date;
-    format?: string;
-    timezone?: string;
-    #date: Date | null;
-
-    constructor(input: string | number, format?: string, timezone?: string) {
-        this.#input = input;
-        this.format = format;
-        this.timezone = timezone;
-        this.#date = null;
-
-      // we should parse it here and not defer to later
-      if(!this.#input) {
-        throw new Error('Input required');
-      }
-
-      if (typeof this.#input == 'number') {
-        this.#date = fromUnixTime(this.#input)
-      } else {
-        if (!this.format) {
-          this.#date = parseISO(this.#input);
-        } else {
-          this.#date = parse(String(this.#input), this.format, new Date())
-        }
-      }
+  #input: string | number | Date | Datetime;  //'2025-01-01 00:00'
+  format?: string;   // 'YYYY-mm-dd HH:mm' Format of the timestamp string
+  timezone?: string; // 'UTC' The timezone of the timestamp string
+  locationTimezone?: string; // UTC 'America/Denver' - the timezone of the measurement location
+  readonly date: DateTime;
 
 
-    }
 
-  toDate() {
-    return this.#date;
+  constructor(
+    input: string | number | Date | Datetime,
+    options?: DatetimeOptionsDefinition
+  ) {
+    if (
+      options?.format?.includes('Z') && options?.timezone
+    ) throw new TypeError()
+    this.#input = input;
+    this.format = options?.format;
+    this.timezone = options?.timezone;
+    this.locationTimezone = options?.locationTimezone ?? options?.timezone;
+    this.date = this.parseDate();
   }
 
-  // this should just be an serializer/formatter
+  private parseDate(): DateTime {
+    if (!this.#input) {
+      throw new TypeError('Input required');
+    }
+    let parsedDate: DateTime | null = null;
+
+    if (typeof this.#input == 'number') {
+      parsedDate = DateTime.fromSeconds(this.#input);
+      if (!this.locationTimezone) this.locationTimezone = 'UTC'
+    } else if (this.#input instanceof Date) {
+      parsedDate = DateTime.fromJSDate(this.#input);
+    }else if (this.#input instanceof Datetime) {
+      parsedDate = this.#input.date;
+    } else {
+      try {
+        if (!this.format) { // defaults to ISO-8601
+          parsedDate = DateTime.fromISO(this.#input);
+        } else {
+          if (this.timezone) {
+            parsedDate = DateTime.fromFormat(this.#input, this.format, { zone: this.timezone })
+          } else {
+            parsedDate = DateTime.fromFormat(this.#input, this.format)
+          }
+        }
+      } catch (error) {
+        throw new TypeError(
+          `Failed to parse date string "${this.#input}" with format "${this.format}". Error: ${error}`
+        );
+      }
+    }
+    if (!parsedDate.isValid) {
+      throw new TypeError(
+        `Invalid date input: "${this.#input}" with format "${this.format}: ${parsedDate.invalidReason}".`
+      );
+    }
+    return parsedDate;
+  }
+
+  toDate(): Date {
+    return this.date.toJSDate();
+  }
+
+  readonly isGreaterThan = (date: Datetime): boolean => this.date > date.date
+
+  readonly isLessThan = (date: Datetime): boolean => this.date < date.date
+
+    // when a null/undefinded value is passed we will always return this.date
+    // this is valuable when you are updating a value that has not been initialized
+  readonly greaterOf = (date: Datetime): Datetime => this.date >= (date?.date ?? this.date) ? this : date
+
+  readonly lesserOf = (date: Datetime): Datetime => this.date <= (date?.date ?? this.date) ? this : date
+
   toString() {
-    return format(this.#date, "yyyy-MM-dd'T'HH:mm:ssxxx");//, { in: tz(this.timezone) });
+    return this.date.setZone(this.locationTimezone).toISO()
+  }
+
+  toUTC() {
+    return this.date.setZone('UTC').toISO()
+  }
+
+  toLocal() {
+    return this.date.setZone().toISO()
   }
 
 }

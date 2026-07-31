fba1116917da5d4670de5124124b0168d0bae079
Russ Biggs <russbiggs@gmail.com>
2026-07-30 20:35:10 -0600

    export types

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,8 +2,12 @@ export type {
 	ClientConfiguration,
 	ConstantValue,
 	DecimalDigitGroup,
+	Parser,
+	ParserOptions,
 	PathExpression,
 	ResourceKeys,
+	SourceRecord,
+	StringParser,
 } from "../types";
 
 export { Datetime } from "./datetime";
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
 export type { ClientConfiguration, ConstantValue } from "./client.ts";
+export type { SourceRecord } from "./data.ts";
 export type { DecimalDigitGroup, PathExpression } from "./metric.ts";
+export type { Parser, ParserOptions, StringParser } from "./parsers.ts";
 export type { ResourceKeys } from "./resource.ts";

154ecff32d75be61e3ac638164d95b67c8601918
Russ Biggs <russbiggs@gmail.com>
2026-08-19 12:56:41 -0600

    format fix

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -1,4 +1,4 @@
 export { json, xml } from "../core/parsers";
 export { BrowserClient } from "./client";
 export { csv, tsv } from "./parsers";
-export { fileReader } from './readers';
+export { fileReader } from "./readers";

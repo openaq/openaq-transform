e6372946b683286606a43129e47af74c1745e5a0
Russ Biggs <russbiggs@gmail.com>
2026-09-05 06:20:39 -0600

    biome fix

--- src/core/client.ts
+++ src/core/client.ts
@@ -1138,7 +1138,7 @@ export abstract class Client<
 
 	/** Class-level {@link info}; instantiates the subclass to read its defaults. */
 	static info<T extends Client>(this: new () => T): ClientInfo {
-		const client = new Client();
+		const client = new this();
 		client.setup();
 		return client.info();
 	}
